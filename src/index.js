df29d23c6bc312a8203df086a4d4bf4b9a628430
wital <witalijmulawa@gmail.com>
2023-01-18 20:51:38 +0100

    feat: final solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  n_string = String(n)
+  n_array = n_string.split("")
+  n_array_reverse = n_array.reverse()
+  n_string_reverse = n_array_reverse.join("")
+  n_reverse = parseInt(n_string_reverse)
+  return n_reverse
 }
+
+
